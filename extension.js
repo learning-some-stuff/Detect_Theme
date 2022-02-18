d740930e1a1960382d0bee914c76d51a33beea85
mike9107 <9107mgarcia@gmail.com>
2022-02-18 16:45:34 -0500

    when executed, changes the theme to the default dark or light vscode theme to match os theme

--- extension.js
+++ extension.js
@@ -11,28 +11,37 @@ const activate = (context) => {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    'detect-theme.helloWorld',
-    () => {
-      let stdout;
-      try {
-        if (os.platform() === 'darwin') {
-          stdout = execSync('defaults read -g AppleInterfaceStyle');
-          if (stdout.toString().trim() === 'Dark') {
-            vscode.window.showInformationMessage('Detected Theme: Dark');
-          }
-        }
-      } catch (error) {
-        if (/does not exist/.test(error.stderr)) {
-          vscode.window.showInformationMessage('Detected Theme: Light');
+  let disposable = vscode.commands.registerCommand('detect-theme.run', () => {
+    const workbenchConfig = vscode.workspace.getConfiguration();
+    let stdout;
+    try {
+      if (os.platform() === 'darwin') {
+        stdout = execSync('defaults read -g AppleInterfaceStyle');
+        if (stdout.toString().trim() === 'Dark') {
+          workbenchConfig.update(
+            'workbench.colorTheme',
+            'Default Dark+',
+            vscode.ConfigurationTarget.Global
+          );
+
+          vscode.window.showInformationMessage('Detected Theme: Dark');
         }
       }
+    } catch (error) {
+      if (/does not exist/.test(error.stderr)) {
+        workbenchConfig.update(
+          'workbench.colorTheme',
+          'Default Light+',
+          vscode.ConfigurationTarget.Global
+        );
+        vscode.window.showInformationMessage('Detected Theme: Light');
+      }
     }
-  );
+  });
   context.subscriptions.push(disposable);
 };
 // this method is called when your extension is deactivated
-const deactivate = () => { };
+const deactivate = () => {};
 
 module.exports = {
   activate,
