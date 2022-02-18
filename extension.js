f2626f3d0fad69bd7fbb94b0e09b368fbf71b32b
mike9107 <9107mgarcia@gmail.com>
2022-02-18 12:31:54 -0500

    initial push, detects dark or ~dark mode for macos(darwin)

---
+++ extension.js
@@ -0,0 +1,36 @@
+const os = require('os');
+const vscode = require('vscode');
+const { execSync } = require('child_process');
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+const activate = (context) => {
+  console.log('Congratulations, your extension "detect-theme" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'detect-theme.helloWorld',
+    () => {
+      let stdout;
+      if (os.platform() === 'darwin') {
+        stdout = execSync('defaults read -g AppleInterfaceStyle');
+        if (stdout.toString().trim() === 'Dark') {
+          vscode.window.showInformationMessage('Detected Theme: Dark');
+        } else {
+          vscode.window.showInformationMessage('Detected Theme: Light');
+        }
+      }
+    }
+  );
+  context.subscriptions.push(disposable);
+};
+// this method is called when your extension is deactivated
+const deactivate = () => {};
+
+module.exports = {
+  activate,
+  deactivate,
+};
